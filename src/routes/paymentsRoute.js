fb1f317dc70219f7ab12f42a45799575d704b2fe
Jarder SIlva <jardersilva@MacBook-Pro-de-Jarder.local>
2020-04-03 16:49:26 -0300

    asdd

--- src/routes/paymentsRoute.js
+++ src/routes/paymentsRoute.js
@@ -8,7 +8,7 @@ routes.get('/checkout/:token/:sandbox/:id/:email/:description/:amount', payments
 routes.get('/success', (req, res) => {
     return res.render('success_screen')
 })
-//res.send(req.get('Teste'));
+
 routes.get('/pending', (req, res) => {
     res.redirect('https://api.mercadopago.com/v1/payments/'+req.query.collection_id+'?access_token='+req.query.token);
 })
